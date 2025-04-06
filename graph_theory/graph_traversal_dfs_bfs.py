6c58a44f61bd4cd0729c08efd058433b487aa5f2
Rohit Mundra <Rohit.Mundra@target.com>
2025-04-07 00:08:58 +0530

    flood-fill or island count problem dfs

--- graph_theory/graph_traversal_dfs_bfs.py
+++ graph_theory/graph_traversal_dfs_bfs.py
@@ -59,8 +59,20 @@ def dfsUsingStack(graph: dict, start_node: str) -> None:
                 visited.add(node)    
     return path
 
-def flood_fill_algo():
-    pass
+def dfs_flood_fill_or_island_count(grid, r, c):
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    # all kinds of boundary conditions or if the cell is visited => pass
+    if (r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] != '1'):
+        return
+    
+    grid[r][c] = '0'  # mark as visited
+    dfs_flood_fill_or_island_count(grid, r + 1, c)
+    dfs_flood_fill_or_island_count(grid, r - 1, c)
+    dfs_flood_fill_or_island_count(grid, r, c + 1)
+    dfs_flood_fill_or_island_count(grid, r, c - 1)
+
 
 
 if __name__ == "__main__":
