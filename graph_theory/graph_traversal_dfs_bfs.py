35c714ba8480d94b6ed53f96ca8ea4910027fe46
Rohit Mundra <Rohit.Mundra@target.com>
2025-04-06 23:53:57 +0530

    bfs and dfs

--- graph_theory/graph_traversal_dfs_bfs.py
+++ graph_theory/graph_traversal_dfs_bfs.py
@@ -6,46 +6,54 @@ class graph:
     def __init__(self, gdict = None):
         self.gdict = gdict if gdict else {}
     
-        
-def dfsRecursion(graph, start, visited = None):
-    """Check for the visited and unvisited nodes
-    RE_CHECK: return is not stable
-    """
-    if visited is None:
-        visited = set()
-        
-    visited.add(start)
-    print(start)
+
+# wrong implementation
+# def dfsRecursion(graph, start):
+#     """Check for the visited and unvisited nodes
+#     RE_CHECK: return is not stable
+#     """        
+#     print(start)
     
-    # graph elements are set so you can subtract the set(visited) from it
-    for next in graph[start] - visited:
-        dfsRecursion(graph, next, visited)
+#     if len(graph[start]) == 0:
+#         return
+    
+#     for val in graph[start]:
+#        dfsRecursion(graph, val) 
 
 
 
-def bfs(graph, startnode):
-    """Track the visited and unvisited nodes using queue"""
+def bfsUsingQueue(graph, startnode):
+    """Track the visited and unvisited nodes using queue
+    visted means that a node will be explored by adding all its adjacent nodes to visited
+    Note - First visted will pop first hence we need FIFO => QUEUE Data Structue for bfs
+    """
     visited = set([startnode]) 
     queue = deque([startnode])
+    path = []
     while queue:
         vertex = queue.popleft()
-        print(vertex)
+        path.append(vertex)
+        
         for node in graph[vertex]:
             if node not in visited:
-                visited.add(node)
                 queue.append(node)
+                visited.add(node)
+    return path
                
                 
 def dfsUsingStack(graph: dict, start_node: str) -> None:
-    """ returns how the graph is traversed using dfs (stack based)"""
+    """ returns how the graph is traversed using dfs (stack based)
+    visted means that a node will be explored by adding all its adjacent nodes to visited
+    Note - First visted will pop last hence we need LIFO => STACK Data Structue for dfs
+    """
     visited = set([start_node])
     stack = deque([start_node])
     path = []
     while stack:
-        temp = stack.pop()
-        path.append(temp)
+        vertex = stack.pop()
+        path.append(vertex)
         
-        for node in graph[temp]:
+        for node in graph[vertex]:
             if node not in visited:
                 stack.append(node)
                 visited.add(node)    
@@ -57,12 +65,12 @@ def flood_fill_algo():
 
 if __name__ == "__main__":
     gdict = { 
-        "a" : set(["b","c"]),
+        "a" : set(["b","c", "e"]),
         "b" : set(["a", "d"]),
         "c" : set(["a", "d"]),
-        "d" : set(["e"]),
-        "e" : set(["a"])
+        "d" : set(["e", "b", "c"]),
+        "e" : set(["a", "d"])
     }
-    dfsRecursion(gdict, 'a')
-    # print(dfsUsingStack(gdict, 'a')) # ['a', 'b', 'd', 'e', 'c']
-    # print(bfs(gdict, "a"))
+    # dfsRecursion(gdict, 'a')
+    print(dfsUsingStack(gdict, 'a')) # ['a', 'b', 'd', 'e', 'c']
+    print(bfsUsingQueue(gdict, "a")) # ['a', 'c', 'b', 'd', 'e']
