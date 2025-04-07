0721f02747c6151e3ac03a9736c049d4503cb5a6
Rohit Mundra <Rohit.Mundra@target.com>
2025-04-07 20:41:40 +0530

    adding graphHasPath function using BFS

--- graph_theory/graph_has_path.py
+++ graph_theory/graph_has_path.py
@@ -1,8 +1,8 @@
 from collections import deque
 from graph_basic import buildDirectedGraph
 
-def directedGraphHasPath(graph: dict, source: int, destination: int) -> bool:
-    """ Directed Graph: Has path from source to destination or not
+def directedGraphHasPathDFS(graph: dict, source: int, destination: int) -> bool:
+    """ Directed Graph: Has path from source to destination or not using DFS
 
     Args:
         graph (dict): _description_
@@ -30,6 +30,34 @@ def directedGraphHasPath(graph: dict, source: int, destination: int) -> bool:
             
     return False
 
+def directedGraphHasPathBFS(graph: dict, source: int, destination: int) -> bool:
+    """ Directed Graph: Has path from source to destination or not using BFS
+
+    Args:
+        graph (dict): _description_
+        source (int): _description_
+        destination (int): _description_
+
+    Returns:
+        bool: _description_
+    """
+    queue = deque([source])
+    visited = set([source])
+   
+    while queue:
+        vertex = queue.popleft()
+        if vertex == destination:
+            return True
+        
+        for v in graph[vertex]:
+            if v == destination:
+                return True
+            
+            if v not in visited:
+                visited.add(v)
+                queue.append(v)
+            
+    return False
 
 if __name__ == "__main__":
     edges_directed = [
@@ -39,7 +67,7 @@ if __name__ == "__main__":
     ]
     directed_graph = buildDirectedGraph(edges_directed)
     print(directed_graph)
-    print(directedGraphHasPath(directed_graph, 100, 500))
+    print(directedGraphHasPathDFS(directed_graph, 100, 500))
     
     # revert edge from 500 -> 40 instead of 40 -> 500
     edges_directed = [
@@ -49,8 +77,13 @@ if __name__ == "__main__":
     ]
     directed_graph = buildDirectedGraph(edges_directed)
     print(directed_graph)
-    print(directedGraphHasPath(directed_graph, 100, 500))
-    print(directedGraphHasPath(directed_graph, 100, 100))
-    print(directedGraphHasPath(directed_graph, 100, 60))
-    print(directedGraphHasPath(directed_graph, 60, 100))
+    print(directedGraphHasPathDFS(directed_graph, 100, 500))
+    print(directedGraphHasPathDFS(directed_graph, 100, 100))
+    print(directedGraphHasPathDFS(directed_graph, 100, 60))
+    print(directedGraphHasPathDFS(directed_graph, 60, 100))
+    
+    print(directedGraphHasPathBFS(directed_graph, 100, 500))
+    print(directedGraphHasPathBFS(directed_graph, 100, 100))
+    print(directedGraphHasPathBFS(directed_graph, 100, 60))
+    print(directedGraphHasPathBFS(directed_graph, 60, 100))
     
